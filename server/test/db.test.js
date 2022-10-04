9c1f7d2507ccd2216b54dc5cf15d36d6d00880bf
ZIshmael <zacharyk.ishmael@gmail.com>
2022-10-04 20:57:00 +0200

    Update db.test.js

--- server/test/db.test.js
+++ server/test/db.test.js
@@ -2,47 +2,8 @@ import assert from 'assert';
 import * as db from '../js/db/db.js'
 
 describe('Testing db functions', () => {
-    describe('findUserByUsername(validUsername:string)', ()=> {
-        it('should return a user', async () => {
-            var username = 'username1'
-            var user = await db.findUserByUsername(username)
-            if (user) {return true}
-            else { throw new Error('No user returned') }
-        })
-    })
-
-    describe('findUserByUsername(invalidUsername:string)', ()=> {
-        it('should return null', async () => {
-            var invalidUsername = 'invlaidUsername'
-            var user = await db.findUserByUsername(invalidUsername)
-            if (!user) {return true}
-            else { throw new Error('User was found when once shouldn\'t be') }
-        })
-    })
-
-    describe('findUserBySessionId(validSessionId:string)', ()=> {
-        it('should return a user', async () => {
-            var validSessionId = 'test-session-id'
-            var user = await db.findUserBySessionId(validSessionId)
-            if (user) {return true}
-            else { throw new Error('No user returned') }
-        })
-    })
-
-    describe('findUserBySessionId(invalidSessionId:string)', ()=> {
-        it('should return null', async () => {
-            var invalidSessionId = 'invalid-test-session-id'
-            var user = await db.findUserBySessionId(invalidSessionId)
-            if (!user) {return true}
-            else { throw new Error('User was found when once shouldn\'t be') }
-        })
-    })
-
-
-
-
-    //TODO //IMPORTANT TO COMPLETE
-    describe('addUser(name,username,password,email,sessionId)', ()=> {
+    //CREATE
+    describe('\n\n\nCREATE - addUser(name,username,password,email,sessionId)', ()=> {
         it('should return the persisted the user', async () => {
             //save new user in database
             
@@ -71,26 +32,80 @@ describe('Testing db functions', () => {
         })
     })
 
-
-    describe('don\'t addUser(name,username,password,email,sessionId)', ()=> {
-        it('should not return a user', async () => {
-            //save new user in database
-            try 
-            {
-                const username = 'invalid-test-username'
-                // await db.addUser(name, username, password, email, testSessionId)
-
-                //check that user was persisted in db'
-                const userCheck = await db.findUserByUsername(username)
-                if (!userCheck) {return true}
-                else { throw new Error('User should not exist in database') }
-            }
-            catch (e)
-            {
-                throw e
-            }
+    //READ
+    describe('\n\n\nREAD - findUserByUsername(validUsername:string)', ()=> {
+        it('should return a user', async () => {
+            var username = 'test-username'
+            var user = await db.findUserByUsername(username)
+            if (user) {return true}
+            else { throw new Error('No user returned') }
         })
     })
 
+    describe('\n\n\n READ - findUserByUsername(invalidUsername:string)', ()=> {
+        it('should return null', async () => {
+            var invalidUsername = 'invlaidUsername'
+            var user = await db.findUserByUsername(invalidUsername)
+            if (!user) {return true}
+            else { throw new Error('User was found when once shouldn\'t be') }
+        })
+    })
 
+    describe('\n\n\n READ - findUserBySessionId(validSessionId:string)', ()=> {
+        it('should return a user', async () => {
+            var validSessionId = 'test-session-id'
+            var user = await db.findUserBySessionId(validSessionId)
+            if (user) {return true}
+            else { throw new Error('No user returned') }
+        })
+    })
+
+    describe('\n\n\nREAD - findUserBySessionId(invalidSessionId:string)', ()=> {
+        it('should return null', async () => {
+            var invalidSessionId = 'invalid-test-session-id'
+            var user = await db.findUserBySessionId(invalidSessionId)
+            if (!user) {return true}
+            else { throw new Error('User was found when once shouldn\'t be') }
+        })
+    })
+
+   
+
+    describe(`\n\n\nDELETE - deleteUser(query:any={username:'test-delete-user'})`, () => {
+        it('should delete a user', async ()=> {
+            
+            try
+            {
+                //add test user to database
+                const name = 'test-name'
+                const username = 'test-delete-user'
+                const password = 'password'
+                const email = 'test-email@email.com'
+                var testSessionId = 'test-session-id'
+                await db.addUser(name, username, password, email, testSessionId)
+
+                //check that user was persisted in db'
+                const userCheck = await db.findUserByUsername(username)
+                if (!userCheck)
+                { throw new Error('User was not persisted in database.') }
+               
+                //delete test user
+                const query = {username: username}
+                const result = await db.deleteUser(query)
+                if(result && result.deletedCount === 1)
+                {
+                    return true
+                }
+                else 
+                {
+                    throw new Error('Test user was not deleted')
+                }
+            }
+            catch(e)
+            {
+                throw e
+            }
+
+        })
+    })
 })
