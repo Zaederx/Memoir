d18d350f101b265a4107c7e744ba6c4c3973ce87
ZIshmael <zacharyk.ishmael@gmail.com>
2022-10-06 19:14:59 +0200

    craete login, logout and signup controller methods

--- server/ts/server.ts
+++ server/ts/server.ts
@@ -2,7 +2,8 @@ import express from 'express'
 import { config as dotEnvConfig } from 'dotenv'
 import { Cookie, createSessionCookie  } from 'simplycookie-js'
 import { v4 as uuidv4 } from 'uuid';
-
+import { authenticateCredentials } from './helpers/login.js'
+import crudDriver from './db/db.js'
 // import * as url from 'url';
 //@ts-ignore
 import path from 'path'
@@ -14,23 +15,28 @@ const server = express();
 const envFilePath = path.join('..', 'demo.env')//only accepts relative path
 console.log('\n',`envFilePath:${envFilePath}`)
 dotEnvConfig({path: envFilePath })
+//@ts-ignore
 const PORT = process.env.PORT
-
+const dbPassword = process.env.DATABASE_PASSWORD
+const uri = `mongodb+srv://memoir-cluster:${dbPassword}@memoir-cluster.g4ldqzg.mongodb.net/?retryWrites=true&w=majority`;
+const db = new crudDriver(uri,'memoir')
 
 server.listen(PORT, () => {
    var message =  '\n'+`Server listening on port:${PORT}`
    console.log(message)
 })
 
+//Note res true means that the action of
+//the server was succesfully carried out
 /*
  * - login
  * - logout
  * - signup
  */
-server.post('/login', (req: any, res: any) => {
+server.post('/login', async (req: any, res: any) => {
    var username = req.body.username
    var password = req.body.password
-   var valid = checkCredentails(username, password)
+   var valid =  await authenticateCredentials(username, password)
    //if valid return auth cookie
    if (valid) 
    {
@@ -48,7 +54,40 @@ server.post('/login', (req: any, res: any) => {
 })
 
 
-function checkCredentails(username:string, password:string):boolean
-{
+server.get('/logout', async (req:any, res:any) => {
+   //set session cookie to null
+   const name = 'memoir-session'
+   const domain = 'localhost'
+   var cookie:Cookie = createSessionCookie(name, null, domain)
+   //set in header
+   res.headers['Set-Cookie'] = cookie.getCookieStr()
+   //and return to client wiht res true
+   res.send({res:true})
+})
 
-}
+server.post('/signup', async (req:any, res:any) => {
+   const name = req.body.name
+   const email = req.body.email
+   const password = req.body.paassword
+   const username = req.body.username
+   const sessionId = uuidv4()//unique id
+   const userExists = await authenticateCredentials(username,password)
+   if (userExists) 
+   {
+      res.send({
+         res:false,
+         message: 'User already exists'
+      })
+   }
+   else
+   {
+      //create user in db
+      await db.addUser(name,username,password,email,sessionId)
+
+      //send response to confirm successful
+      res.send({
+         res:true,
+         message:'user added successfully'
+      })
+   }
+})
