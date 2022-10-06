dbfe40433dce4a1ed3f9830b6379afe6910d22fb
ZIshmael <zacharyk.ishmael@gmail.com>
2022-10-06 19:19:11 +0200

    main.ts - add views

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { BootstrapVue3} from 'bootstrap-vue-3'
 import * as vues from './vue-imports'
 import router from './router'
 
@@ -8,13 +7,17 @@ import './assets/main.css'
 const app = createApp(vues.App)
 
 //add components
-app.component('home', vues.Home)
-app.component('about', vues.About)
-app.component('scrapbook', vues.Scrapbook)
-
+app.component('Home', vues.Home)
+app.component('About', vues.About)
+app.component('Scrapbook', vues.Scrapbook)
+app.component('AjaxAuth', vues.AjaxAuth)
+app.component('Banner', vues.Banner)
+app.component('HomeMain', vues.HomeMain)
+app.component('SiteFooter', vues.SiteFooter)
+app.component('UploadForm', vues.UploadForm)
+app.component('HeadScripts', vues.HeadScripts)
 
 app.use(createPinia())
 app.use(router)
-app.use(BootstrapVue3)
 
 app.mount('#app')
