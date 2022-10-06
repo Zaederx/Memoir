773116784545c87deca33376b459861e4de35bd3
ZIshmael <zacharyk.ishmael@gmail.com>
2022-10-06 19:16:13 +0200

    Update vue-imports.ts

--- client/src/vue-imports.ts
+++ client/src/vue-imports.ts
@@ -1,6 +1,12 @@
-import App from './App.vue'
-import Home from './views/Home.vue'
-import Scrapbook from './components/Scrapbook.vue'
-import About from './views/About.vue'
+import AjaxAuth from '@/components/AjaxAuth.vue'
+import App from '@/App.vue'
+import HeadScripts from '@/components/HeadScripts.vue'
+import Home from '@/views/Home.vue'
+import Scrapbook from '@/components/Scrapbook.vue'
+import About from '@/views/About.vue'
+import HomeMain from '@/components/HomeMain.vue'
+import SiteFooter from '@/components/SiteFooter.vue'
+import Banner from '@/components/Banner.vue'
+import UploadForm from '@/components/UploadForm.vue'
 
-export {App, Home, Scrapbook, About}
+export {App, AjaxAuth, HeadScripts,Banner, Home, HomeMain, SiteFooter, Scrapbook, About, UploadForm}
