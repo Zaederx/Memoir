27a181083b7093592328a6304145fcd8c651a0a1
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2023-07-28 14:44:02 +0200

    Create SignUp.vue

---
+++ client/src/views/SignUp.vue
@@ -0,0 +1,19 @@
+<!-- JS & VUE -->
+<script setup lang="ts">
+
+</script>
+
+<!-- HTML -->
+<template>
+   <HeadScripts></HeadScripts>
+   <Banner heading1="Chat" heading2="SignUp View"></Banner>
+   <HomeMain>
+    <SignUpForm></SignUpForm>
+   </HomeMain>
+   <SiteFooter></SiteFooter>
+</template>
+
+<!-- CSS -->
+<style scoped>
+@import url('@/assets/base.css')
+</style>
