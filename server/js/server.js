f258d8dbe99dda2df6f748bb94a45a21270a3610
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2023-07-28 14:58:20 +0200

    server - server.ts - added parsers and CSRF Protection + response handler + refactoring

--- server/js/server.js
+++ server/js/server.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import csrf from 'csurf';
 import { config as dotEnvConfig } from 'dotenv';
 import { createSessionCookie } from 'simplycookie-js';
 import { v4 as uuidv4 } from 'uuid';
@@ -7,6 +11,7 @@ import crudDriver from './db/db.js';
 // import * as url from 'url';
 //@ts-ignore
 import path from 'path';
+import { printFormatted } from 'printformatted-js';
 const server = express();
 // const __filename = url.fileURLToPath(import.meta.url);
 // const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
@@ -19,9 +24,37 @@ const PORT = process.env.PORT;
 const dbPassword = process.env.DATABASE_PASSWORD;
 const uri = `mongodb+srv://memoir-cluster:${dbPassword}@memoir-cluster.g4ldqzg.mongodb.net/?retryWrites=true&w=majority`;
 const db = new crudDriver(uri, 'memoir');
+const clientDOMAIN = 'https://localhost:5173';
+var csrfProtection = csrf({ cookie: true });
+server.use(cors({
+    credentials: true,
+    origin: clientDOMAIN
+}));
+server.use(bodyParser.urlencoded({ extended: true })); //for parsing application/x-www-form-urlencoded
+// server.use(multer.array());//for parsing multipart form data
+server.use(bodyParser.json());
+server.use(cookieParser()); //for parsing cookies
+server.use(csrfProtection);
+server.disable('x-powered-by'); //remove defualt express header ad
 server.listen(PORT, () => {
     var message = '\n' + `Server listening on port:${PORT}`;
     console.log(message);
+});
+server.get('/csrf-token', (req, res) => {
+    //set access control origin
+    const name = 'Access-Control-Allow-Origin'; //
+    const value = clientDOMAIN; //clientDOMAIN
+    res.setHeader(name, value);
+    //set access control credentials
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    //set csrf cookie
+    // const cookieName = 'csrfToken'
+    // const cookieValue = req.csrfToken()
+    // var cookie = getAppCookie(cookieName,cookieValue,'localhost')
+    // res.setHeader('Set-Cookie', [cookie.getCookieStr()])
+    // cookie.print()
+    //also add it in in the json for retrieval in js - needed to insert into meta tag
+    return res.json({ csrfToken: req.csrfToken() });
 });
 //Note res true means that the action of
 //the server was succesfully carried out
@@ -31,8 +64,10 @@ server.listen(PORT, () => {
  * - signup
  */
 server.post('/login', async (req, res) => {
+    printFormatted('blue', 'response handler for "/login" called');
     var username = req.body.username;
     var password = req.body.password;
+    printFormatted('yellow', 'username:', username, '\npassword:', password);
     var valid = await authenticateCredentials(username, password);
     //if valid return auth cookie
     if (valid) {
@@ -41,13 +76,14 @@ server.post('/login', async (req, res) => {
         const domain = 'localhost';
         var cookie = createSessionCookie(name, value, domain);
         res.headers['Set-Cookie'] = cookie.getCookieStr();
-        res.send({ res: true });
+        res.send({ res: true, message: 'User credentials were valid' });
     }
     else {
-        res.send({ res: false });
+        res.send({ res: false, message: 'User credentials were invalid' });
     }
 });
 server.get('/logout', async (req, res) => {
+    printFormatted('blue', 'response handler "/logout" called');
     //set session cookie to null
     const name = 'memoir-session';
     const domain = 'localhost';
@@ -57,13 +93,23 @@ server.get('/logout', async (req, res) => {
     //and return to client wiht res true
     res.send({ res: true });
 });
-server.post('/signup', async (req, res) => {
+server.post('/sign-up', async (req, res) => {
+    printFormatted('blue', 'response handler "/sign-up" called');
     const name = req.body.name;
     const email = req.body.email;
-    const password = req.body.paassword;
+    const password1 = req.body.password1;
+    const password2 = req.body.password2;
     const username = req.body.username;
     const sessionId = uuidv4(); //unique id
-    const userExists = await authenticateCredentials(username, password);
+    //print request body parameters
+    printFormatted('yellow', 'name:', name, '\n', 'email', email, '\n', 'password1', password1, '\n', 'password2', password2, '\n', 'username', username, '\n', 'sessionId', sessionId);
+    if (password1 != password2) {
+        res.send({
+            res: false,
+            message: 'Passwords do not match.'
+        });
+    }
+    const userExists = await authenticateCredentials(username, password1);
     if (userExists) {
         res.send({
             res: false,
@@ -71,8 +117,13 @@ server.post('/signup', async (req, res) => {
         });
     }
     else {
-        //create user in db
-        await db.addUser(name, username, password, email, sessionId);
+        try {
+            //create user in db
+            await db.addUser(name, username, password1, email, sessionId);
+        }
+        catch (error) {
+            printFormatted('red', 'Error adding user to database:', error);
+        }
         //send response to confirm successful
         res.send({
             res: true,
--- server/ts/server.ts
+++ server/ts/server.ts
@@ -1,4 +1,8 @@
 import express from 'express'
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import csrf from 'csurf'
 import { config as dotEnvConfig } from 'dotenv'
 import { Cookie, createSessionCookie  } from 'simplycookie-js'
 import { v4 as uuidv4 } from 'uuid';
@@ -7,6 +11,7 @@ import crudDriver from './db/db.js'
 // import * as url from 'url';
 //@ts-ignore
 import path from 'path'
+import { printFormatted } from 'printformatted-js';
 const server = express();
 
 // const __filename = url.fileURLToPath(import.meta.url);
@@ -14,17 +19,55 @@ const server = express();
 //allows you to use .env file for additional env variables
 const envFilePath = path.join('..', 'demo.env')//only accepts relative path
 console.log('\n',`envFilePath:${envFilePath}`)
-dotEnvConfig({path: envFilePath })
+dotEnvConfig({ path: envFilePath })
 //@ts-ignore
 const PORT = process.env.PORT
 const dbPassword = process.env.DATABASE_PASSWORD
 const uri = `mongodb+srv://memoir-cluster:${dbPassword}@memoir-cluster.g4ldqzg.mongodb.net/?retryWrites=true&w=majority`;
 const db = new crudDriver(uri,'memoir')
+const clientDOMAIN = 'https://localhost:5173'
 
+var csrfProtection = csrf({cookie:true})
+server.use(cors(
+   {
+       credentials:true,
+       origin: clientDOMAIN
+   }
+))
+server.use(bodyParser.urlencoded({ extended:true }))//for parsing application/x-www-form-urlencoded
+// server.use(multer.array());//for parsing multipart form data
+server.use(bodyParser.json())
+server.use(cookieParser())//for parsing cookies
+server.use(csrfProtection)
+
+server.disable('x-powered-by')//remove defualt express header ad
 server.listen(PORT, () => {
    var message =  '\n'+`Server listening on port:${PORT}`
    console.log(message)
 })
+
+
+
+
+server.get('/csrf-token', (req:any, res:any) => {
+   //set access control origin
+   const name = 'Access-Control-Allow-Origin'//
+   const value:string = clientDOMAIN//clientDOMAIN
+   res.setHeader(name,value)
+   //set access control credentials
+   res.setHeader('Access-Control-Allow-Credentials','true')
+   //set csrf cookie
+   // const cookieName = 'csrfToken'
+   // const cookieValue = req.csrfToken()
+   // var cookie = getAppCookie(cookieName,cookieValue,'localhost')
+   // res.setHeader('Set-Cookie', [cookie.getCookieStr()])
+   // cookie.print()
+
+   //also add it in in the json for retrieval in js - needed to insert into meta tag
+   return res.json({csrfToken:req.csrfToken()})
+})
+
+
 
 //Note res true means that the action of
 //the server was succesfully carried out
@@ -34,27 +77,38 @@ server.listen(PORT, () => {
  * - signup
  */
 server.post('/login', async (req: any, res: any) => {
+   printFormatted('blue', 'response handler for "/login" called')
    var username = req.body.username
    var password = req.body.password
-   var valid =  await authenticateCredentials(username, password)
-   //if valid return auth cookie
-   if (valid) 
+   printFormatted('yellow', 'username:',username, '\npassword:',password)
+   
+   try 
    {
-      const name = 'memoir-session'
-      const value = uuidv4()//sessionId
-      const domain = 'localhost'
-      var cookie:Cookie = createSessionCookie(name, value, domain)
-      res.headers['Set-Cookie'] = cookie.getCookieStr()
-      res.send({res:true})
-   }
-   else
+      var valid =  await authenticateCredentials(username, password)
+      //if valid return auth cookie
+      if (valid) 
+      {
+         const name = 'memoir-session'
+         const value = uuidv4()//sessionId
+         const domain = 'localhost'
+         var cookie:Cookie = createSessionCookie(name, value, domain)
+         res.headers['Set-Cookie'] = cookie.getCookieStr()
+         res.send({res:true, message:'User credentials were valid'})
+      }
+      else
+      {
+         res.send({res:false, message:'User credentials were invalid'})
+      }
+   } catch (error) 
    {
-      res.send({res:false})
+      printFormatted('red', error)
    }
+   
 })
 
 
 server.get('/logout', async (req:any, res:any) => {
+   printFormatted('blue', 'response handler "/logout" called')
    //set session cookie to null
    const name = 'memoir-session'
    const domain = 'localhost'
@@ -65,13 +119,35 @@ server.get('/logout', async (req:any, res:any) => {
    res.send({res:true})
 })
 
-server.post('/signup', async (req:any, res:any) => {
+server.post('/sign-up', async (req:any, res:any) => {
+   printFormatted('blue', 'response handler "/sign-up" called')
    const name = req.body.name
    const email = req.body.email
-   const password = req.body.paassword
+   const password1 = req.body.password1
+   const password2 = req.body.password2
    const username = req.body.username
    const sessionId = uuidv4()//unique id
-   const userExists = await authenticateCredentials(username,password)
+
+
+   //print request body parameters
+   printFormatted('yellow', 
+   'name:',name,'\n',
+   'email',email,'\n',
+   'password1', password1,'\n',
+   'password2', password2,'\n',
+   'username', username,'\n',
+   'sessionId', sessionId
+   )
+
+
+   if (password1 != password2) 
+   {
+      res.send({
+         res:false,
+         message:'Passwords do not match.'
+      })
+   }
+   const userExists = await authenticateCredentials(username,password1)
    if (userExists) 
    {
       res.send({
@@ -79,10 +155,19 @@ server.post('/signup', async (req:any, res:any) => {
          message: 'User already exists'
       })
    }
+   
    else
    {
-      //create user in db
-      await db.addUser(name,username,password,email,sessionId)
+      try
+      {
+         //create user in db
+         await db.addUser(name,username,password1,email,sessionId)
+      }
+      catch (error)
+      {
+         printFormatted('red', 'Error adding user to database:', error)
+      }
+      
 
       //send response to confirm successful
       res.send({
